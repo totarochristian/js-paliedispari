8b1045659d145866c926ebeeafe8e843193020fa
totarochristian <totaro.christian.18@gmail.com>
2023-03-16 16:13:06 +0100

    Add function GetRandomInt in the utility script

--- js/utility.js
+++ js/utility.js
@@ -30,3 +30,15 @@ function CheckEvenOdd(){
     let isEven = document.getElementById("evenOddSelect").value == "even";
     
 }
+
+/**
+ * Function that will generate a random int
+ * @param {bigint} max Max value of the interval
+ * @param {bigint} min Min value of the interval
+ * @returns {bigint} Generated random int value
+ */
+function GetRandomInt(max,min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
